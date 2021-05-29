a38ba86f351445441b0c96c08ac11564de5a5d70
Himanshu72 <36104578+Himanshu72@users.noreply.github.com>
2021-05-29 18:15:40 +0530

    reg working

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const db=require("../utility/db");
+
+const path = require('path');
 /* GET home page. */
 router.get('/', async function(req, res, next) {
  
@@ -36,10 +38,26 @@ router.get('/registration', function(req, res, next) {
 router.post("/reg",async (req,res)=>{
   try{  
   console.log(req.body);
-    req.body.name={};
+  //console.log(req.file.fieldname);
+  console.log(req.files);
+  req.body.name={};
     req.body.name.fname=req.body.fname;
     req.body.name.fname=req.body.lname;
-    await db.insertDoc(req.body);  
+    let username="d"+req.body.phone
+    req.files.proof.mv(path.join(__dirname, '../public/data/', username+"proof"), (err) => {
+      if (err) throw err;
+    });
+    
+    req.files.photo.mv(path.join(__dirname, '../public/data/', username+"photo"), (err) => {
+      if (err) throw err;
+    });
+
+    req.files.MRN.mv(path.join(__dirname, '../public/data/', username+"MRN"), (err) => {
+      if (err) throw err;
+    });
+   await db.insertDoc(req.body);  
+
+
     res.redirect("/login");
   }catch(err){
     console.log(err);
